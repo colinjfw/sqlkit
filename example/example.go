976e847a0e99d0bc5610c7616401d80bfee21cb1
Colin Walker <colinwalker270@gmail.com>
2018-04-03 06:36:25 -0700

    Improve Docs (#12)
    * Improve examples.
    * Improve badges and other readme items.

--- example/example.go
+++ example/example.go
@@ -18,23 +18,27 @@ func uuid() string {
 	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
+// Base is a base type for database objects.
 type Base struct {
 	ID        string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
+// User is a database object.
 type User struct {
 	Base
 	Email string
 }
 
+// UserRepo represents the user repository.
 type UserRepo struct {
 	db db.DB
 }
 
 func (rep *UserRepo) table() string { return "users" }
 
+// Save a user.
 func (rep *UserRepo) Save(ctx context.Context, u *User) error {
 	var stmt db.SQL
 	if u.ID == "" {
@@ -42,11 +46,12 @@ func (rep *UserRepo) Save(ctx context.Context, u *User) error {
 		stmt = rep.db.Insert().Into(rep.table()).Record(u)
 	} else {
 		u.UpdatedAt = time.Now()
-		stmt = rep.db.Update(rep.table()).Record(u)
+		stmt = rep.db.Update(rep.table()).Record(u).Where("id = ?", u.ID)
 	}
 	return rep.db.Exec(ctx, stmt).Err()
 }
 
+// Get will fetch a user.
 func (rep *UserRepo) Get(ctx context.Context, id string) (*User, error) {
 	u := &User{}
 	err := rep.db.Query(
@@ -56,6 +61,7 @@ func (rep *UserRepo) Get(ctx context.Context, id string) (*User, error) {
 	return u, err
 }
 
+// List will list a set of users.
 func (rep *UserRepo) List(ctx context.Context) ([]*User, error) {
 	u := []*User{}
 	err := rep.db.Query(
