40450bc2d032253709fd48c4b1c290795d83f81f
Colin Walker <colinwalker270@gmail.com>
2019-07-15 14:51:54 +0000

    String builders.

--- db/dialect_generic.go
+++ db/dialect_generic.go
@@ -27,79 +27,112 @@ func (m genericMapper) rollbackSavepoint(name string) string {
 }
 
 func (m genericMapper) query(q SelectStmt) string {
-	var sql string
-	sql += "SELECT " + strings.Join(q.columns, ",") + " "
-	sql += "FROM " + q.table + " "
+	sql := strings.Builder{}
+	sql.WriteString("SELECT ")
+	sql.WriteString(strings.Join(q.columns, ","))
+	sql.WriteString(" FROM ")
+	sql.WriteString(q.table)
+
 	for _, join := range q.join {
-		sql += join[0] + " JOIN " + join[1] + " ON " + join[2] + " "
+		sql.WriteString(join[0])
+		sql.WriteString(" JOIN ")
+		sql.WriteString(join[1])
+		sql.WriteString(" ON ")
+		sql.WriteString(join[2])
 	}
 	if q.where != "" {
-		sql += "WHERE " + q.where + " "
+		sql.WriteString(" WHERE ")
+		sql.WriteString(q.where)
 	}
 	if q.groupBy != nil {
-		sql += "GROUP BY " + strings.Join(q.groupBy, ", ") + " "
+		sql.WriteString(" GROUP BY ")
+		sql.WriteString(strings.Join(q.groupBy, ", "))
 	}
 	if q.orderBy != nil {
-		sql += "ORDER BY " + strings.Join(q.orderBy, ", ") + " "
+		sql.WriteString(" ORDER BY ")
+		sql.WriteString(strings.Join(q.orderBy, ", "))
 	}
 	if q.limit != "" {
-		sql += "LIMIT " + q.limit + " "
+		sql.WriteString(" LIMIT ")
+		sql.WriteString(q.limit)
 	}
 	if q.offset != "" {
-		sql += "OFFSET " + q.offset + " "
+		sql.WriteString(" OFFSET ")
+		sql.WriteString(q.offset)
 	}
-	return rebind(m.bindType, sql)
+	return rebind(m.bindType, sql.String())
 }
 
 func (m genericMapper) delete(q DeleteStmt) string {
-	var sql string
-	sql += "DELETE FROM " + q.sel.table + " "
+	sql := strings.Builder{}
+	sql.WriteString("DELETE FROM ")
+	sql.WriteString(q.sel.table)
+	sql.WriteString(" ")
 	for _, join := range q.sel.join {
-		sql += join[0] + " JOIN " + join[1] + " ON " + join[2] + " "
+		sql.WriteString(join[0])
+		sql.WriteString(" JOIN ")
+		sql.WriteString(join[1])
+		sql.WriteString(" ON ")
+		sql.WriteString(join[2])
+		sql.WriteString(" ")
 	}
 	if q.sel.where != "" {
-		sql += "WHERE " + q.sel.where + " "
+		sql.WriteString("WHERE ")
+		sql.WriteString(q.sel.where)
+		sql.WriteString(" ")
 	}
-	return rebind(m.bindType, sql)
+	return rebind(m.bindType, sql.String())
 }
 
 func (m genericMapper) insert(q InsertStmt) string {
-	var sql string
-	sql += "INSERT INTO " + q.table + " "
-	sql += "(" + strings.Join(q.columns, ", ") + ") "
-	sql += "VALUES "
+	sql := strings.Builder{}
+	sql.WriteString("INSERT INTO ")
+	sql.WriteString(q.table)
+	sql.WriteString(" (")
+	sql.WriteString(strings.Join(q.columns, ", "))
+	sql.WriteString(") VALUES ")
 	for i, row := range q.rows {
-		sql += questions(len(row))
+		sql.WriteString(questions(len(row)))
 		if i != len(q.rows)-1 {
-			sql += ", "
+			sql.WriteString(", ")
 		}
 	}
-	return rebind(m.bindType, sql)
+	return rebind(m.bindType, sql.String())
 }
 
 func (m genericMapper) update(q UpdateStmt) string {
-	var sql string
-	sql += "UPDATE " + q.table + " SET "
+	sql := strings.Builder{}
+	sql.WriteString("UPDATE ")
+	sql.WriteString(q.table)
+	sql.WriteString(" SET ")
 	for i := range q.columns {
-		sql += q.columns[i] + "=?"
+		sql.WriteString(q.columns[i])
+		sql.WriteString("=?")
 		if i == len(q.columns)-1 {
-			sql += " "
+			sql.WriteString(" ")
 		} else {
-			sql += ", "
+			sql.WriteString(", ")
 		}
 	}
 	if q.sel.where != "" {
-		sql += "WHERE " + q.sel.where
+		sql.WriteString("WHERE ")
+		sql.WriteString(q.sel.where)
 	}
-	return rebind(m.bindType, sql)
+	return rebind(m.bindType, sql.String())
 }
 
 func questions(count int) string {
-	qs := []string{}
+	qs := strings.Builder{}
+	qs.WriteString("(")
 	for i := 0; i < count; i++ {
-		qs = append(qs, "?")
+		if i == count - 1 {
+			qs.WriteString("?")
+		} else {
+			qs.WriteString("?, ")
+		}
 	}
-	return "(" + strings.Join(qs, ", ") + ")"
+	qs.WriteString(")")
+	return qs.String()
 }
 
 const (
